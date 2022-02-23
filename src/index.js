b5587699fd7d4b7d4165e63a7fa267f7524943fe
Luis Manuel Alvarez Bautista <lmalvarez@personalsoft.com.co>
2022-02-22 21:42:32 -0500

    version 1 del proyecto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,93 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import '../src/components/Card';
+import data from '../src/data.json';
 
-console.log('Happy hacking :)')
+
+const tabcontent = document.getElementsByClassName("tabcontent");
+
+const getInfo = ()=>{
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+}
+
+getInfo();
+
+
+function openPage(evt, cityName) {
+  
+  getInfo();
+
+
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+const $btnExperience = document.getElementById('btnExperience');
+const $btnpPojects = document.getElementById('btnpPojects');
+const $btncontact = document.getElementById('btncontact');
+
+const container = document.getElementById('container');
+
+$btnExperience.addEventListener('click', (event)=>{
+
+  const experiences = data.info.categories.experiences;
+
+  createNode(experiences, 'experiences');
+
+});
+
+
+$btnpPojects.addEventListener('click', (event)=>{
+
+  
+  const projects = data.info.categories.projects;
+
+  createNode(projects, 'projects');
+});
+
+
+$btncontact.addEventListener('click', (event)=>{
+  openPage(event, 'contact');
+});
+
+//
+
+function createNode(array, id){
+
+  clearNodes();
+
+  const arrayNode = document.createElement('div');
+  arrayNode.className = 'tabcontent';
+  arrayNode.id = id;
+
+  array.forEach( element => {
+
+    const cardNode     = document.createElement('card-component');
+    cardNode.titleName   = element.title;
+    cardNode.time   = element.time;
+    cardNode.subtitle   = element.subtitle;
+    cardNode.description = element.description;
+    cardNode.link        = element.link;
+    cardNode.tecnologies = element.tecnologies;
+
+    arrayNode.append(cardNode);
+  
+  })
+
+  container.appendChild(arrayNode);
+
+
+}
+
+function clearNodes(){
+  const expe = document.getElementById('experiences');
+  const proj = document.getElementById('projects');
+
+  if(expe){
+    expe.remove();
+  }
+
+  if(proj){
+    proj.remove();
+  }
+}
